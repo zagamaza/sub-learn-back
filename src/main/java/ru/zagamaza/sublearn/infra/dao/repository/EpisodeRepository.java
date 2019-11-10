9d07b3b9c841a0236bd5af0e42dc362eb19f5817
almazic91 <almazic91@gmail.cm>
2019-11-10 15:46:47 +0300

    [NF] SUBLEARN-38 reworked a query for calculate out percent learned words

--- src/main/java/ru/zagamaza/sublearn/infra/dao/repository/EpisodeRepository.java
+++ src/main/java/ru/zagamaza/sublearn/infra/dao/repository/EpisodeRepository.java
@@ -23,20 +23,22 @@ public interface EpisodeRepository extends JpaRepository<EpisodeEntity, Long> {
     @Query(value = "select e from EpisodeEntity e join fetch e.worldEntities where e.id = :id")
     Optional<EpisodeEntity> findByIdWithWords(@Param("id") Long id);
 
-    @Query(value = "select distinct e.season from episodes e where e.collection_id = :collectionId order by season", nativeQuery = true)
+    @Query(value = "select distinct e.season from episodes e where e.collection_id = :collectionId order by season",
+           nativeQuery = true)
     List<Integer> getSeasonsByCollectionId(@Param("collectionId") Long collectionId);
 
-    @Query(value = "select (select count(ewe.world_entities_id)\n" +
+    @Query(value = "select (select  sum(case when uw.rate < 6 then cast(uw.rate as decimal) / 5 else 1 end)\n" +
             "        from episodes e\n" +
             "                 join episodes_world_entities ewe on e.id = ewe.episode_entity_id\n" +
             "                 join words w on ewe.world_entities_id = w.id\n" +
             "                 join user_word uw on w.id = uw.word_id\n" +
-            "        where e.id = :id and uw.user_id = :userId\n" +
-            "          and uw.rate > 5) * 100 / (select case when count(1) = 0 then null else count(1) end\n" +
-            "                                    from episodes e\n" +
-            "                                             join episodes_world_entities ewe on e.id = ewe.episode_entity_id\n" +
-            "                                             join words w on ewe.world_entities_id = w.id\n" +
-            "                                    where e.id = :id);", nativeQuery = true)
+            "        where e.id = :id\n" +
+            "          and uw.user_id = :userId) * 100 " +
+            "       / (select case when count(1) = 0 then null else count(1) end\n" +
+            "                                             from episodes e\n" +
+            "                                                      join episodes_world_entities ewe on e.id = ewe.episode_entity_id\n" +
+            "                                                      join words w on ewe.world_entities_id = w.id\n" +
+            "                                             where e.id = :id);", nativeQuery = true)
     Integer getLearnedPercent(@Param("id") Long id, @Param("userId") Long userId);
 
 }
